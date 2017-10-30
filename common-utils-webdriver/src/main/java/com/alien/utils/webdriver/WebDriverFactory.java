10fa3c02cfd0d422e9f3271acaaa2e083313d54d
Paton, Andrew <andrew.paton@sky.uk>
2017-10-30 07:54:35 +0000

    updated to use WebDriverManager from bonigarcia (github)

--- common-utils-webdriver/src/main/java/com/alien/utils/webdriver/WebDriverFactory.java
+++ common-utils-webdriver/src/main/java/com/alien/utils/webdriver/WebDriverFactory.java
@@ -15,6 +15,11 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public final class WebDriverFactory {
 	
@@ -35,7 +40,7 @@ public final class WebDriverFactory {
     private static String webDriver;
 
 
-    public static WebDriver getWebDriver() throws IOException{
+    public static WebDriver getWebDriver() throws IOException, InstantiationException, IllegalAccessException{
     	
         if (System.getProperty("web.driver") != null) {
             webDriver = System.getProperty("web.driver");
@@ -45,15 +50,26 @@ public final class WebDriverFactory {
                 
         if (WEB_DRIVER == null) {
         	
-        		switch (webDriver){
+        	Class<? extends WebDriver> driverClass;
         	
-        		case CHROME: WEB_DRIVER = getChromeDriver(); break;
-        		case FIREFOX: WEB_DRIVER = getFirefoxDriver(); break;
-        		case PHANTOMJS: WEB_DRIVER = getPhantomJSDriver(); break;
+        	switch (webDriver){
         	
-        		default: WEB_DRIVER = getFirefoxDriver();
-        	        	
-        		}
+//        		case CHROME: WEB_DRIVER = getChromeDriver(); break;
+//        		case FIREFOX: WEB_DRIVER = getFirefoxDriver(); break;
+//        		case PHANTOMJS: WEB_DRIVER = getPhantomJSDriver(); break;
+//        	
+//        		default: WEB_DRIVER = getFirefoxDriver();
+        	        
+        		case CHROME: driverClass = ChromeDriver.class; break;
+        		case FIREFOX: driverClass = FirefoxDriver.class; break;
+        		case PHANTOMJS: driverClass = PhantomJSDriver.class; break;
+        	
+        		default: driverClass = FirefoxDriver.class;
+        	}
+
+        		WebDriverManager.getInstance(driverClass).setup();
+        		
+        		WEB_DRIVER = driverClass.newInstance();
 
         		WEB_DRIVER.manage().window().maximize();
 
@@ -75,68 +91,68 @@ public final class WebDriverFactory {
         return (WEB_DRIVER != null);
     }
     
-    private static String getAbsolutePath(String file){
-
-    		ClassLoader classLoader = WebDriver.class.getClassLoader();
-    		String path = new File(classLoader.getResource(file).getFile()).getAbsolutePath();
-    	
-    		LOGGER.debug("Path to WebDriver : " + path);
-    	
-    		return path;
-    }
-    
-    private static WebDriver getChromeDriver(){
-
-        final File chromeDriverFile = new File(getAbsolutePath(CHROME_DRIVER_PATH));
-        if (chromeDriverFile.exists() && !chromeDriverFile.canExecute()) {
-            chromeDriverFile.setExecutable(true);
-        }
-    	        
-        System.setProperty("webdriver.chrome.driver", getAbsolutePath(CHROME_DRIVER_PATH));
-        
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        return new ChromeDriver(capabilities);
-    }
-    
-    private static WebDriver getFirefoxDriver(){
-    	
-        System.setProperty("webdriver.gecko.driver", getAbsolutePath(GECKO_DRIVER_PATH));
-
-    	    FirefoxProfile profile = new FirefoxProfile();
-    	    profile.setAlwaysLoadNoFocusLib(true);
-    	    return new FirefoxDriver(profile);
-    }
-    
-    private static WebDriver getPhantomJSDriver() throws IOException{
-        DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
-
-        ArrayList<String> cliArgsCap = createCliArgs();
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "loadImages", true);
-        capabilities.setCapability("takesScreenshot", true);
-        capabilities.setJavascriptEnabled(true);
-     
-        final File phantomjsDriverFile = new File(getAbsolutePath(PHANTOMJS_DRIVER_PATH));
-        if (phantomjsDriverFile.exists() && !phantomjsDriverFile.canExecute()) {
-    	        phantomjsDriverFile.setExecutable(true);
-        }
-
-        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, getAbsolutePath(PHANTOMJS_DRIVER_PATH));
-
-        return new PhantomJSDriver(capabilities);
-    }
-
-    private static ArrayList<String> createCliArgs() {
-        ArrayList<String> cliArgsCap = new ArrayList<>();
-        cliArgsCap.add("--ignore-ssl-errors=true");
-        cliArgsCap.add("--web-security=false");
-        cliArgsCap.add("--disk-cache=true");
-        cliArgsCap.add("--max-disk-cache-size=256");
-        cliArgsCap.add("--proxy-type=none");
-        return cliArgsCap;
-    }
+//    private static String getAbsolutePath(String file){
+//
+//    		ClassLoader classLoader = WebDriver.class.getClassLoader();
+//    		String path = new File(classLoader.getResource(file).getFile()).getAbsolutePath();
+//    	
+//    		LOGGER.debug("Path to WebDriver : " + path);
+//    	
+//    		return path;
+//    }
+//    
+//    private static WebDriver getChromeDriver(){
+//
+//        final File chromeDriverFile = new File(getAbsolutePath(CHROME_DRIVER_PATH));
+//        if (chromeDriverFile.exists() && !chromeDriverFile.canExecute()) {
+//            chromeDriverFile.setExecutable(true);
+//        }
+//    	        
+//        System.setProperty("webdriver.chrome.driver", getAbsolutePath(CHROME_DRIVER_PATH));
+//        
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("start-maximized");
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//        return new ChromeDriver(capabilities);
+//    }
+//    
+//    private static WebDriver getFirefoxDriver(){
+//    	
+//        System.setProperty("webdriver.gecko.driver", getAbsolutePath(GECKO_DRIVER_PATH));
+//
+//    	    FirefoxProfile profile = new FirefoxProfile();
+//    	    profile.setAlwaysLoadNoFocusLib(true);
+//    	    return new FirefoxDriver(profile);
+//    }
+//    
+//    private static WebDriver getPhantomJSDriver() throws IOException{
+//        DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+//
+//        ArrayList<String> cliArgsCap = createCliArgs();
+//        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, cliArgsCap);
+//        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "loadImages", true);
+//        capabilities.setCapability("takesScreenshot", true);
+//        capabilities.setJavascriptEnabled(true);
+//     
+//        final File phantomjsDriverFile = new File(getAbsolutePath(PHANTOMJS_DRIVER_PATH));
+//        if (phantomjsDriverFile.exists() && !phantomjsDriverFile.canExecute()) {
+//    	        phantomjsDriverFile.setExecutable(true);
+//        }
+//
+//        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, getAbsolutePath(PHANTOMJS_DRIVER_PATH));
+//
+//        return new PhantomJSDriver(capabilities);
+//    }
+//
+//    private static ArrayList<String> createCliArgs() {
+//        ArrayList<String> cliArgsCap = new ArrayList<>();
+//        cliArgsCap.add("--ignore-ssl-errors=true");
+//        cliArgsCap.add("--web-security=false");
+//        cliArgsCap.add("--disk-cache=true");
+//        cliArgsCap.add("--max-disk-cache-size=256");
+//        cliArgsCap.add("--proxy-type=none");
+//        return cliArgsCap;
+//    }
 
 }
